23430d519985ec2ada95ab748c13e40dd0093dcb
Zokam-LS <lucassy07@outlook.com>
2022-12-20 16:41:51 -0500

    main archives created

---
+++ Protes_Project/src/app.js
@@ -0,0 +1,19 @@
+/* const express = require("express");
+const app = express();
+const path = require('path')
+
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+})
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+
+
+app.listen(9999, () => {
+    console.log("corriendo en el puerto 9999");
+}) */
